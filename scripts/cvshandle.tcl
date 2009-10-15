d7c755c83d7165e4e7942181b8a7c4f0594a17d2
ramsan <ramsan>
2009-10-15 12:21:10 +0000

    Small correction

--- scripts/cvshandle.tcl
+++ scripts/cvshandle.tcl
@@ -1008,19 +1008,22 @@ proc RamDebugger::CVS::update_recursive_cmd { w what args } {
 		if { $ret eq "cancel" } { return }
 	    }
 	    set pwd [pwd]
-	    lassign "" cvs_files_dict fossil_files_dict
+	    lassign "" cvs_files_dict fossil_files_dict items
 	    foreach item $sel_ids {
 		set dir [$tree item text [$tree item parent $item] 0]
 		if { [regexp {^[MA]\s(\S+)} [$tree item text $item 0] {} file] } { 
 		    dict lappend cvs_files_dict $dir $file
+		    dict set items [list cvs $dir $file] $item
 		} elseif { [regexp {(\w{2,})\s+(.*)} [$tree item text $item 0] {} mode file] && $mode ne "UNCHANGED" } {
 		    dict lappend fossil_files_dict $dir $file
+		    dict set items [list fossil $dir $file] $item
 		}
 	    }
 	    dict for "dir fs" $cvs_files_dict {
 		cd $dir
 		set err [catch { exec cvs commit -m $message {*}$fs 2>@1 } ret]
 		if { $err } { set color red } else { set color blue }
+		set item [dict get $items [list cvs $dir $file]]
 		$tree item element configure $item 0 e_text_sel -fill $color -text $ret
 	    }
 	    dict for "dir fs" $fossil_files_dict {
@@ -1030,6 +1033,7 @@ proc RamDebugger::CVS::update_recursive_cmd { w what args } {
 		cd $dir
 		set err [catch { exec fossil commit --nosign -m $message {*}$fs 2>@1 } ret]
 		if { $err } { set color red } else { set color blue }
+		set item [dict get $items [list fossil $dir $file]]
 		$tree item element configure $item 0 e_text_sel -fill $color -text $ret
 	    }
 	    cd $pwd
