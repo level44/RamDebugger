584e795996b7d46f469b9e25438d20fb8bbb4e3c
ramsan <ramsan>
2012-12-08 16:06:03 +0000

    adapted sizes in dramdebugger2

--- utils/dramdebugger2.tcl
+++ utils/dramdebugger2.tcl
@@ -1,7 +1,11 @@
 
+cd $env(HOME)
+
 if { [winfo screenheight .] < 1024 } {
-    set g1 720x845+0+0
-    set g2 750x600-0+0
+    set g1 700x845+0+0
+    set g2 654x525+715+0
+#     set g1 720x845+0+0
+#     set g2 750x600-0+0
 } else {
     set g1 699x970+0+0
     set g2 550x600-0+0
@@ -15,6 +19,8 @@ if { [lindex $argv 0] eq "-b" } {
 
 set wish [info nameofexecutable]
 
-exec $wish $env(HOME)/mytcltk/RamDebugger/RamDebugger.tcl -rgeometry $g1 -big_icons $big_icons {*}$argv &
-exec $wish $env(HOME)/mytcltk/RamDebugger/RamDebugger.tcl -rgeometry $g2 -big_icons $big_icons -prefs_group win2 &
+exec $wish $env(HOME)/mytcltk/RamDebugger/RamDebugger.tcl -rgeometry $g1 \
+    -big_icons $big_icons {*}$argv &
+exec $wish $env(HOME)/mytcltk/RamDebugger/RamDebugger.tcl -rgeometry $g2 \
+    -big_icons $big_icons -prefs_group win2 &
 exit
