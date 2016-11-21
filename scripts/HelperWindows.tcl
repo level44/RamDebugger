6cd381b29ac97638bad7cf4a1efdda6ec16f02d4
ramsan <ramsan>
2016-11-21 11:52:41 +0000

    small correction

--- scripts/HelperWindows.tcl
+++ scripts/HelperWindows.tcl
@@ -1982,7 +1982,9 @@ proc RamDebugger::GotoLine {} {
 
     set w [dialogwin_snit $active_text._ask -title [_ "Goto line"] -class $::className]
     set f [$w giveframe]
-    wm attributes $w -type dialog
+    if { $::tcl_platform(platform) eq "unix" } {
+	wm attributes $w -type dialog
+    }
 
     ttk::label $f.l -text [_ "Go to line:"]
     spinbox $f.sb -from 1 -to 10000 -increment 1 -textvariable [$w give_uservar line] \
